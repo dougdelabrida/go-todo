cec3f47b531fcd0c4dfbab488a0140b0349e2491
Douglas Delabrida <dougdelabrida@gmail.com>
2022-12-03 11:05:00 +0000

    test: add tests for /todos

--- backend/main_test.go
+++ backend/main_test.go
@@ -1,27 +1,44 @@
 package main
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-func TestHomeRouter(t *testing.T) {
-	req, err := http.NewRequest("GET", "/", nil)
+func TestListTodosEmpty(t *testing.T) {
+	rr, err := recorderHandler("GET", "/todos", nil, RetrieveTodosHandler)
 
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	rr := httptest.NewRecorder()
+	expected := "[]"
 
-	handler := http.HandlerFunc(HelloHandler)
+	assertResponse("unexpected status code", rr.Code, http.StatusOK, t)
 
-	handler.ServeHTTP(rr, req)
+	assertResponse("unexpected Content-Type", rr.Header().Get("Content-Type"), "application/json", t)
 
-	expected := "Hello World"
+	assertResponse("handler returned unexpected body", strings.TrimSpace(rr.Body.String()), expected, t)
+}
 
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+func assertResponse(description string, received any, expected any, t *testing.T) {
+	if received != expected {
+		t.Errorf("%v: %v, but got %v", description, expected, received)
 	}
+}
+
+func recorderHandler(method string, url string, body io.Reader, handlerFunc http.HandlerFunc) (*httptest.ResponseRecorder, error) {
+	req, err := http.NewRequest(method, url, body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handlerFunc)
+	handler.ServeHTTP(rr, req)
+	return rr, err
 }
