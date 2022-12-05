18412bde6fcc7861125fcdce79ead1d27452eedf
Douglas Delabrida <dougdelabrida@gmail.com>
2022-12-05 11:43:11 +0000

    feat: load todos from the server

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,28 +1,32 @@
-import { Container } from '@chakra-ui/react'
+import { useCallback, useEffect, useState } from 'react'
+
+import { Container, Text } from '@chakra-ui/react'
 
 import AddTodo from '../components/AddTodo'
 import TodoList from '../components/TodoList'
+import { fetchTodos } from '../data'
+import { Todo } from '../types'
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const fetchData = useCallback(async () => {
+    setIsLoading(true)
+    const data = await fetchTodos()
+    setTodos(data)
+    setIsLoading(false)
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <Container>
       <AddTodo />
-      <TodoList
-        data={[
-          {
-            _id: 'test1',
-            text: 'First Todo',
-            status: 0,
-            priority: 4,
-          },
-          {
-            _id: 'test2',
-            text: 'Second Todo',
-            status: 1,
-            priority: 4,
-          },
-        ]}
-      />
+      <TodoList data={todos} />
+      {isLoading && <Text>Loading...</Text>}
     </Container>
   )
 }
--- frontend/tests/__tests__/pages/index_spec.tsx
+++ frontend/tests/__tests__/pages/index_spec.tsx
@@ -1,10 +1,46 @@
-import { render } from '@testing-library/react'
+import { render, waitFor } from '@testing-library/react'
+import fetchMock from 'jest-fetch-mock'
 
 import Home from '../../../pages'
+import { renderWithChakra } from '../../testUtils'
+
+fetchMock.enableMocks()
+fetchMock.dontMock()
+
+jest.mock('../../../data', () => ({
+  fetchTodos: jest.fn().mockImplementation(() => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(dataMock)
+      }, 0)
+    })
+  }),
+}))
 
 describe('Index page', () => {
   it('render properly', () => {
     const tree = render(<Home />)
     expect(tree).toBeTruthy()
   })
+
+  it('should fetch data and render todos', async () => {
+    const tree = renderWithChakra(<Home />)
+
+    await waitFor(() => {
+      expect(tree.getByText('Loading...')).toBeTruthy()
+    })
+
+    await waitFor(() => {
+      expect(tree.getByText(dataMock[0].text)).toBeTruthy()
+      expect(tree.getByText(dataMock[1].text)).toBeTruthy()
+      expect(tree.getByText(dataMock[2].text)).toBeTruthy()
+      expect(tree.queryByText('Loading...')).toBeFalsy()
+    })
+  })
 })
+
+const dataMock = [
+  { _id: '638c2ac852b3b9e763b7f5f6', text: 'Say what', status: 2, priority: 1 },
+  { _id: '638c2b0135fcde43f3a22000', text: 'Looks like it worked', status: 2, priority: 1 },
+  { _id: '638ca7a1e9265c7255f4c063', text: 'Check me out', status: 2, priority: 1 },
+]
